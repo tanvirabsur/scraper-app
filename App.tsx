fd7b8414491f51c1f2a9d82ab544dab4a47dc45d
Tanvir <tanvirabsar@yahoo.com>
2026-07-02 11:44:59 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import Styles from './style/styling'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, TextInput, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+    <SafeAreaView style={Styles.body}>
+      <View >
+        <Text style={Styles.greenText}>Hello, World!</Text>
+
+        <TextInput
+          placeholder="Enter text here..."
+          style={Styles.input}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
---
+++ style/styling.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+   body: {
+      padding: 20,
+   },
+   input: {
+      borderWidth: 1,
+      borderColor: 'gray',
+      padding: 10,
+      marginBottom: 10,
+   },
+   greenText: {
+        color: 'green',
+   }
+})
+
+export default styles;
